74eb9e99f7ae37f51b2c0522eeb94fe6f0aecfdb
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-11-02 23:47:02 +0100

    safe

--- src/pages/account/Deposite.jsx
+++ src/pages/account/Deposite.jsx
@@ -597,9 +597,11 @@ const Deposite = () => {
                                     </p>
                                 </div>
                             </a>
-                            <button className="btn btn-small m-2 text-light border rounded">Copy Address</button>
-                            <button style={{background: "#25C866"}} data-bs-toggle="modal" data-bs-target="#share" className="btn m-2 btn-small text-dark">Share Address</button>
+                           <div className="btn-group">
+                           <button className="btn btn-small text-light border rounded">Copy Address</button>
+                            <button style={{background: "#25C866"}} data-bs-toggle="modal" data-bs-target="#share" className="btn btn-small text-dark">Share Address</button>
                         </div>
+                           </div>
                     </div>
 
 
